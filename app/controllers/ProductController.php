3b3993bc660ef94d5d8fb1bd78811b9c3d8ba22f
Md. Ashiquzzaman <ashiquzzaman33@gmail.com>
2015-05-01 17:47:09 +0600

    Update Product controller

--- app/controllers/ProductController.php
+++ app/controllers/ProductController.php
@@ -8,38 +8,21 @@ class ProductController extends BaseController{
 	public function postAddNewProduct(){
 		$barcode = Input::get("barcode");
 		$quantity = Input::get("quantity");
-
-		$category = Input::get("category");
-		$supplier = Input::get("supplier");
-		$purchase_price = Input::get("purchase_price");
-		$sell_price = Input::get("sell_price");
-
-		for($i=0; $i<$quantity; $i++){
-
-				DB::table('products')->insert(
-   			 		array(
-			   			 	'barcode'			=> $barcode+$i, 
-			   			 	'category'			=> $category,
-			   			 	'suppllier'			=> $supplier,
-			   			 	'purchase_price'	=> $purchase_price,
-			   			 	'sell_price' 		=> $sell_price
-   			 			)
-				);
-		}
+		$category = Input::get('category');
+/*
 		DB::table('all_products_purchase')->insert(
 			array(
 			    	    'category'				=> $category,
-			       	    'suppllier'				=> $supplier,
 			   			'purchase_price'		=> $purchase_price,
 			   		 	'sell_price' 			=> $sell_price,
 			   		 	'quantity'				=> $quantity,
 			   		 	'total_purchase_price'	 => $purchase_price*$quantity
 				)
-			);
+			);*/
 		$old =  DB::table('categories')->select('quantity')
-		->where('name', '=', $category)->first();
+		->where('barcode', '=', $barcode)->first();
 
-		DB::table('categories')->where('name', $category)
+		DB::table('categories')->where('barcode', $barcode)
                ->update(array('quantity' => $old->quantity+$quantity));
 
 		return Redirect::route('getAddNewProduct')->with('msg', 'Products have been added successfully');
@@ -48,18 +31,20 @@ class ProductController extends BaseController{
 	public function getSearchProduct(){
 		return View::make('partials.search_product');
 	}
-	public function getProductByCategory($category){
-		$res = DB::table('products')->select(DB::raw('date, category, COUNT(*) AS quantity,
-					 suppllier, purchase_price, sell_price'))
-			
-					->where('category', '=', $category)
-                    ->groupBy(DB::raw('CAST(date AS DATE)'))
-                    ->get();
 
+	public function getProductByCategory($category){
+		$res = DB::table('categories')->select(DB::raw('barcode, category, unit'))
+					->where('category', '=', $category)->get();
         return json_encode($res);
 
 	}
-	public function postEditProduct(){
+	public function getProductByBarcode($barcode){
+		$res = DB::table('categories')->select(DB::raw('barcode, category, unit'))
+				->where('barcode', '=', $barcode)->get();
+        return json_encode($res);
+
+	}
+	/*public function postEditProduct(){
 		DB::table('products')
             ->where('category', '=', Input::get('category'))
             ->whereBetween('date', array(Input::get('date'), Input::get('date')." "."23:59:59"))
@@ -69,7 +54,7 @@ class ProductController extends BaseController{
             	 ));
             return "Success";
 
-	}
+	}*/
 
 
 }
