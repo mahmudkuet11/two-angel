8f24ffad0b259721856e01e439229ddad8cb5bf9
Mahmud Raju <mahmudkuet11@gmail.com>
2015-05-07 16:33:59 +0600

    exe added

--- app/views/partials/index.blade.php
+++ app/views/partials/index.blade.php
@@ -2,6 +2,8 @@
 
 @section('content')
 
-
+<div style="width:50%;min-height:50%;display:block;margin:0 auto;margin-top:50px;">
+<img src="images/logo1.png" alt="" style="width:100%;height:100%">
+</div>
 
 @stop
